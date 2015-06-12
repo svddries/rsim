841688f9d5c353e4dfbd26e66cf8ed25bf9f5dea
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-06-12 16:54:37 +0200

    polypartition is now a submodule

--- install-deps.bash
+++ install-deps.bash
@@ -5,5 +5,3 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 git submodule init
 git submodule update
-
-svn checkout http://polypartition.googlecode.com/svn/trunk/ $DIR/external/polypartition -r 10
